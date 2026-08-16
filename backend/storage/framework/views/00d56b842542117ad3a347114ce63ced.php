8c8fb7fd4fc2451717a4e9b19a7d3b040985c0c3
devlops-survive <ethic345@gmail.com>
2026-08-16 08:52:58 -0700

    Stop tracking Laravel generated files

--- backend/storage/framework/views/00d56b842542117ad3a347114ce63ced.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\Users\Y.com\Downloads\sacco-management-implemented (1)\backend\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
--- backend/storage/framework/views/ebc3a54dc220f1ded54e3a83642a0c72.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\Y.com\Downloads\sacco-management-implemented (1)\backend\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
