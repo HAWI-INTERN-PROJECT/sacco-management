e44e22648fe34ed7c18c22c917610917ede5f9a5
Gemechu Alemu <alemugemechu44@gmail.com>
2026-08-31 10:19:39 +0300

    fix(ci): resolve PHPStan property error and wrap LoginPage tests with QueryClientProvider

--- backend/app/Http/Resources/V1/UserResource.php
+++ backend/app/Http/Resources/V1/UserResource.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property-read string $username
  * @property-read string|null $role
  * @property-read int|null $sacco_id
+ * @property-read \App\Models\Sacco|null $sacco
  * @property-read Carbon|null $email_verified_at
  * @property-read string|null $phone
  * @property-read string $role
